08da4f2c67ca70689b4445e6b0079c2f6f2b47f1
Renan Correa <renanhenrikecorrea@hotmail.com>
2025-02-05 06:52:36 +0000

    Cep

--- FormacaoJavaDeveloper/src/estruturasExcepcionais/CepExemple.java
+++ FormacaoJavaDeveloper/src/estruturasExcepcionais/CepExemple.java
@@ -1,21 +1,19 @@
 package estruturasExcepcionais;
 
-public class CepExemple {
+public class CepExemple  {
     public static void main(String[] args) {
         try {
-            String cepFormatdo = newCep("23765064");
-            System.out.println(cepFormatdo);
+            String cepFormatado = formatarCep("23765064");
+            System.out.println(cepFormatado);
         } catch (CepInvalidoException e) {
             System.out.println("Try again.");
         }
-        
-    
 
     }
 
-    static String newCep (String cep) throws CepInvalidoException{
+    static String formatarCep (String cep) throws CepInvalidoException {
         if(cep.length() != 8 )
-            throw new CepInvalidoException();
+            throw new CepInvalidoException ();
 
             //Trying to get one CEP.
             return "23.765-064";
